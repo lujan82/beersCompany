a8f16cfa3ee7e82db5ec6bb527ea4d084dc8ec51
Carlos Luján <lujanbenita@gmail.com>
2023-02-04 20:06:53 +0100

    created vsc file to project

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -1,37 +1,37 @@
+import ExternalLinks from "./externalLinks"
 import Languages from "./languages"
 import NavLink from "./navLink"
-import ExternalLinks from "./externalLinks"
 
 const Nav = ({ user, isOpen }) => {
-  //TODO: data for pages
+	//TODO: data for pages
 
-  return (
-    <nav className={isOpen ? "open nav" : "nav"}>
-      <Languages />
-      <div className="nav__container-img">
-        <img src="/images/logo.png" alt="" />
-      </div>
-      <ul>
-        <NavLink path={"/"} text="home" />
-        <NavLink path={"/our-beers"} text="ourBeers" />
-        <NavLink path={"/premium-beers"} text="premiumBeers" />
-        <NavLink path={"/jobs"} text="jobs" />
-        <NavLink path={"/history"} text="history" />
-        <NavLink path={"/wt"} text="wt" />
-        <NavLink path={"/contact"} text="contact" />
-        <NavLink path={"/shop"} text="shop" />
-        <NavLink path={"/checkout"} text="checkout" />
+	return (
+		<nav className={isOpen ? "open nav" : "nav"}>
+			<Languages />
+			<div className="nav__container-img">
+				<img src="/images/logo.png" alt="" />
+			</div>
+			<ul>
+				<NavLink path={"/"} text="home" />
+				<NavLink path={"/our-beers"} text="ourBeers" />
+				<NavLink path={"/premium-beers"} text="premiumBeers" />
+				<NavLink path={"/jobs"} text="jobs" />
+				<NavLink path={"/history"} text="history" />
+				<NavLink path={"/wt"} text="wt" />
+				<NavLink path={"/contact"} text="contact" />
+				<NavLink path={"/shop"} text="shop" />
+				<NavLink path={"/checkout"} text="checkout" />
 
-        {user?.email !== "" ? (
-          <NavLink path={"/login"} text="logout" />
-        ) : (
-          <NavLink path={"/login"} text="login" />
-        )}
-      </ul>
+				{user?.email !== "" ? (
+					<NavLink path={"/login"} text="logout" />
+				) : (
+					<NavLink path={"/login"} text="login" />
+				)}
+			</ul>
 
-      <ExternalLinks />
-    </nav>
-  )
+			<ExternalLinks />
+		</nav>
+	)
 }
 
 export default Nav
--- data/beers.js
+++ data/beers.js
@@ -1,122 +1,122 @@
 export const BEER = [
-  {
-    id: 1,
-    url: "/images/beer1.jpg",
-    urlName: "hoptimus-prime",
-    name: "Hoptimus Prime",
-    pack: "Pack 12 ",
-    price: 49,
-    description:
+	{
+		id: 1,
+		url: "/images/beer1.jpg",
+		urlName: "hoptimus-prime",
+		name: "Hoptimus Prime",
+		pack: "Pack 12 ",
+		price: 49,
+		description:
       "Casting the shadow of the forest, breathing its aroma with a hint of caramel, this APA reinterprets the american classic while inviting you to connect to the good things in life.",
-    ingredients: "Water/Barley/Hops/Yeast ",
-    alc: "5.2%",
-    ml: "330",
-    premium: false,
-  },
-  {
-    id: 2,
-    url: "/images/beer2.jpg",
-    urlName: "peter-parker",
-    name: "Peter Parker",
-    pack: "Pack 8 ",
-    price: 49,
-    description:
+		ingredients: "Water/Barley/Hops/Yeast ",
+		alc: "5.2%",
+		ml: "330",
+		premium: false,
+	},
+	{
+		id: 2,
+		url: "/images/beer2.jpg",
+		urlName: "peter-parker",
+		name: "Peter Parker",
+		pack: "Pack 8 ",
+		price: 49,
+		description:
       "Love your fate. Embrace chaos, forget reason. Follow a path with the only certainty; you don't know where it will lead you. The elegant and fruity bitterness of this IPA is only awaiting for you to get its",
-    ingredients:
+		ingredients:
       " Water, Cereals, Hops (Colombus, Belma, Chinook, Centenial, Cascade, Mosaïc), Yeast",
-    alc: "4.8%",
-    ml: "330",
-    premium: false,
-  },
-  {
-    id: 3,
-    url: "/images/beer3.jpg",
-    urlName: "guybrush-threepwo",
-    name: "Guybrush Threepwood",
-    pack: "Pack 6 ",
-    price: 60,
-    description:
+		alc: "4.8%",
+		ml: "330",
+		premium: false,
+	},
+	{
+		id: 3,
+		url: "/images/beer3.jpg",
+		urlName: "guybrush-threepwo",
+		name: "Guybrush Threepwood",
+		pack: "Pack 6 ",
+		price: 60,
+		description:
       "Standing tall in the face of the classics, this unfiltered lager with modern hopping rates expresses the perfect balance between tradition & innovation. Petite, but only by its name ",
-    ingredients:
+		ingredients:
       "Water, Malts, Hops (Northern Brewer, Saaz, Mittelfruh), Yeast ",
-    alc: "5.0%",
-    ml: "350",
-    premium: false,
-  },
-  {
-    id: 4,
-    url: "/images/beer5.jpg",
-    urlName: "infinite-8",
-    name: "Infinite 8",
-    pack: "Pack 8 ",
-    price: 45,
-    description:
+		alc: "5.0%",
+		ml: "350",
+		premium: false,
+	},
+	{
+		id: 4,
+		url: "/images/beer5.jpg",
+		urlName: "infinite-8",
+		name: "Infinite 8",
+		pack: "Pack 8 ",
+		price: 45,
+		description:
       "Casting the shadow of the forest, breathing its aroma with a hint of caramel, this APA reinterprets the american classic while inviting you to connect to the good things in life.",
-    ingredients: "Water, Barley, Oats, Wheat, Hops, Yeast.",
-    alc: "5.3%",
-    ml: "350",
-    premium: false,
-  },
-  {
-    id: 5,
-    url: "/images/beer6.jpg",
-    urlName: "rye-river",
-    name: "Rye River",
-    pack: "Pack 10 ",
-    price: 59,
-    description:
+		ingredients: "Water, Barley, Oats, Wheat, Hops, Yeast.",
+		alc: "5.3%",
+		ml: "350",
+		premium: false,
+	},
+	{
+		id: 5,
+		url: "/images/beer6.jpg",
+		urlName: "rye-river",
+		name: "Rye River",
+		pack: "Pack 10 ",
+		price: 59,
+		description:
       "Bringing craft beer to new heights, this new collaboration with the Freeride World Tour brings a fresh perspective on taste in the FWT brand. Packed with juicy and fruity hop aromas while extremely crushable, the 3.5% Alc. Vol. Session IPA will leave you begging for another one!",
-    ingredients:
+		ingredients:
       "Water, Malts, Hops (Northern Brewer, Saaz, Mittelfruh), Yeast ",
-    alc: "5.6%",
-    ml: "350",
-    premium: false,
-  },
-  {
-    id: 6,
-    url: "/images/beer7.jpg",
-    urlName: "mediterranean-levante",
-    name: "Mediterranean Levante",
-    pack: "Pack 12 ",
-    price: 39,
-    description:
+		alc: "5.6%",
+		ml: "350",
+		premium: false,
+	},
+	{
+		id: 6,
+		url: "/images/beer7.jpg",
+		urlName: "mediterranean-levante",
+		name: "Mediterranean Levante",
+		pack: "Pack 12 ",
+		price: 39,
+		description:
       "Carried by a soft cloud, Pillows is a beer born from lucid dreams: carefree and audacious. Its perfect balance between soft & juicy palate will spark your imagination",
-    ingredients:
+		ingredients:
       "Water, Cereals, Hops (Nothern Brewer, Belma, Cluster, Mandarina Bavaria), Yeast",
-    alc: "5.2%",
-    ml: "200",
-    premium: false,
-  },
-  {
-    id: 7,
-    url: "/images/beer8.jpg",
-    urlName: "number-five",
-    name: "Number Five",
-    pack: "Pack 5 ",
-    price: 49,
-    description:
+		alc: "5.2%",
+		ml: "200",
+		premium: false,
+	},
+	{
+		id: 7,
+		url: "/images/beer8.jpg",
+		urlName: "number-five",
+		name: "Number Five",
+		pack: "Pack 5 ",
+		price: 49,
+		description:
       "Carried by a soft cloud, Pillows is a beer born from lucid dreams: carefree and audacious. Its perfect balance between soft & juicy palate will spark your imagination",
-    ingredients:
+		ingredients:
       "Water, Malts, Hops (Northern Brewer, Saaz, Mittelfruh), Yeast ",
-    alc: "5.0%",
-    ml: "500",
-    premium: false,
-  },
-  {
-    id: 8,
-    url: "/images/beer4.jpg",
-    urlName: "crew-republic",
-    name: "Crew Republic",
-    pack: "Pack 12 ",
-    price: 70,
-    description:
+		alc: "5.0%",
+		ml: "500",
+		premium: false,
+	},
+	{
+		id: 8,
+		url: "/images/beer4.jpg",
+		urlName: "crew-republic",
+		name: "Crew Republic",
+		pack: "Pack 12 ",
+		price: 70,
+		description:
       "Casting the shadow of the forest, breathing its aroma with a hint of caramel, this APA reinterprets the american classic while inviting you to connect to the good things in life.",
-    ingredients:
+		ingredients:
       "Water, Cereals, Hops (Nothern Brewer, Belma, Cluster, Mandarina Bavaria), Yeast",
-    alc: "5.5%",
-    ml: "330",
-    premium: false,
-  },
+		alc: "5.5%",
+		ml: "330",
+		premium: false,
+	},
 ]
 
 export default BEER
