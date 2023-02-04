8d1cbcb7610b7523f5a6aeef88faa048422d5d8c
Carlos Luján <lujanbenita@gmail.com>
2023-02-04 13:21:04 +0100

    fixed details post with hydrated state

--- pages/blog/[title].js
+++ pages/blog/[title].js
@@ -6,86 +6,93 @@ import MainLayout from "../../components/layouts/mainLayout"
 import { POSTS } from "../../data/posts"
 
 const nextPrevPost = currentId => {
-	const prev = POSTS.find(i => i.id === currentId - 1)
-	const next = POSTS.find(i => i.id === currentId + 1)
+  const prev = POSTS.find(i => i.id === currentId - 1)
+  const next = POSTS.find(i => i.id === currentId + 1)
 
-	return {
-		prev,
-		next,
-		total: POSTS.length,
-	}
+  return {
+    prev,
+    next,
+    total: POSTS.length,
+  }
 }
 
 const PostDetail = () => {
-	const router = useRouter()
+  const router = useRouter()
 
-	const [post, setPost] = useState()
-	const [nextPrev, setNextPrev] = useState()
+  const [post, setPost] = useState()
+  const [nextPrev, setNextPrev] = useState()
 
-	useEffect(() => {
-		const currentPost = POSTS.find(i => i.path === router.asPath)
-		setPost(currentPost)
-		if (currentPost) return setNextPrev(nextPrevPost(currentPost.id))
-	}, [router])
+  const [hydrated, setHydrated] = useState(false)
+  useEffect(() => {
+    setHydrated(true)
+  }, [])
 
-	return (
-		<MainLayout className="isfixed">
-			{post !== undefined && (
-				<div className="post">
-					<Link href="/blog">
-						<a className="comeback">Back</a>
-					</Link>
+  useEffect(() => {
+    const currentPost = POSTS.find(i => i.path === router.asPath)
+    setPost(currentPost)
+    if (currentPost) return setNextPrev(nextPrevPost(currentPost.id))
+  }, [router])
 
-					<div
-						className="head-page"
-						style={{ backgroundImage: `url(${post.img})` }}
-					>
-						<div className="container__head">
-							<div>
-								<h1>BLOG</h1>
-								<h3>Onto your next adventure</h3>
-							</div>
-						</div>
-					</div>
+  if (!hydrated) {
+    // Returns null on first render, so the client and server match
+    return null
+  }
 
-					<div className="post__content">
-						<h4>{post.date}</h4>
-						<h2>{post.title}</h2>
-						<p className="post__text">{post.content}</p>
-					</div>
-
-					<div className="post__prev-next-container">
-						<Link
-							href={nextPrev.prev !== undefined ? nextPrev.prev.path : ""}
-							legacyBehavior
-						>
-							<a
-								className={
-									nextPrev.prev === undefined ? "inactive" : "post__prev-next"
-								}
-							>
-								{" "}
+  return (
+    <MainLayout className="isfixed">
+      {post !== undefined && (
+        <div className="post">
+          <Link href="/blog" legacyBehavior>
+            <a className="comeback">Back</a>
+          </Link>
+          <div
+            className="head-page"
+            style={{ backgroundImage: `url(${post.img})` }}
+          >
+            <div className="container__head">
+              <div>
+                <h1>BLOG</h1>
+                <h3>Onto your next adventure</h3>
+              </div>
+            </div>
+          </div>
+          <div className="post__content">
+            <h4>{post.date}</h4>
+            <h2>{post.title}</h2>
+            <p className="post__text">{post.content}</p>
+          </div>
+          <div className="post__prev-next-container">
+            <Link
+              href={nextPrev.prev !== undefined ? nextPrev.prev.path : ""}
+              legacyBehavior
+            >
+              <a
+                className={
+                  nextPrev.prev === undefined ? "inactive" : "post__prev-next"
+                }
+              >
+                {" "}
                 Previous{" "}
-							</a>
-						</Link>
-						<Link
-							href={nextPrev.next !== undefined ? nextPrev.next.path : ""}
-							legacyBehavior
-						>
-							<a
-								className={
-									nextPrev.next === undefined ? "inactive" : "post__prev-next"
-								}
-							>
-								{" "}
+              </a>
+            </Link>
+            <Link
+              href={nextPrev.next !== undefined ? nextPrev.next.path : ""}
+              legacyBehavior
+            >
+              <a
+                className={
+                  nextPrev.next === undefined ? "inactive" : "post__prev-next"
+                }
+              >
+                {" "}
                 Next{" "}
-							</a>
-						</Link>
-					</div>
-				</div>
-			)}
-		</MainLayout>
-	)
+              </a>
+            </Link>
+          </div>
+        </div>
+      )}
+    </MainLayout>
+  )
 }
 
 export default PostDetail
