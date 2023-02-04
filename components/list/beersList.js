c8e738212c7555f9f820fc91531ccb92734352dc
Carlos Luján <lujanbenita@gmail.com>
2023-02-04 12:59:27 +0100

    improve background images for 4k screens and remove premium beers in our-beers page

--- components/list/beersList.js
+++ components/list/beersList.js
@@ -1,19 +1,19 @@
 import Link from "next/link"
 
 const BeersList = ({ i, isPremium }) => {
-	return isPremium ? (
-		<Link href={`./beers/${i.urlName}`}>
-			<div className="premium-beers__item">
-				<img src={i.url} alt="" />
-			</div>
-		</Link>
-	) : (
-		<div className="beers__item">
-			<Link href={`./beers/${i.urlName}`}>
-				<img src={i.url} alt="" />
-			</Link>
-		</div>
-	)
+  return isPremium ? (
+    <Link href={`./beers/${i.urlName}`}>
+      <div className="premium-beers__item">
+        <img src={i.url} alt="" />
+      </div>
+    </Link>
+  ) : (
+    <div className="beers__item">
+      <Link href={`./beers/${i.urlName}`}>
+        <img src={i.url} alt="" />
+      </Link>
+    </div>
+  )
 }
 
 export default BeersList
--- pages/shop.js
+++ pages/shop.js
@@ -12,75 +12,74 @@ import { absoluteUrl, getAppCookies, verifyToken } from "../middleware/utils"
 import { updateCart } from "../redux/shop/shopAction"
 
 const Shop = props => {
-	const [t] = useTranslation("shop")
-	const dispatch = useDispatch()
-	const cartRedux = useSelector(state => state.cart)
+  const [t] = useTranslation("shop")
+  const dispatch = useDispatch()
+  const cartRedux = useSelector(state => state.cart)
 
-	const [cart, setCart] = useState({
-		packs: 0,
-		total: 0,
-		infoText: "",
-		items: [],
-	})
+  const [cart, setCart] = useState({
+    packs: 0,
+    total: 0,
+    infoText: "",
+    items: [],
+  })
 
-	useEffect(() => {
-		if (cart.packs == 0 && cartRedux.packs > 0) {
-			setCart(cartRedux)
-		}
-	}, [])
+  useEffect(() => {
+    if (cart.packs == 0 && cartRedux.packs > 0) {
+      setCart(cartRedux)
+    }
+  }, [])
 
-	useEffect(() => {
-		if (cartRedux.packs >= cart.packs) return
-		dispatch(updateCart(cart))
-	}, [cart.items.length == 0])
+  useEffect(() => {
+    if (cartRedux.packs >= cart.packs) return
+    dispatch(updateCart(cart))
+  }, [cart.items.length == 0])
 
-	useEffect(() => {
-		dispatch(updateCart(cart))
-	}, [cart])
+  useEffect(() => {
+    dispatch(updateCart(cart))
+  }, [cart])
 
-	return (
-		<MainLayout props={props} className={"isfixed"}>
-			<div className="shop-page">
-				<ShoppingCart cart={cart} />
+  return (
+    <MainLayout props={props} className={"isfixed"}>
+      <div className="shop-page">
+        <ShoppingCart cart={cart} />
 
-				<div className="shop__container">
-					<h2>{t("ourProducts")}</h2>
-					<div className="shop__beers">
-						{BEER.map((i, index) => (
-							<ShopList i={i} key={index} setCart={setCart} cart={cart} />
-						))}
-					</div>
-				</div>
+        <div className="shop__container">
+          <h2>{t("ourProducts")}</h2>
+          <div className="shop__beers">
+            {BEER.map((i, index) => (
+              <ShopList i={i} key={index} setCart={setCart} cart={cart} />
+            ))}
+          </div>
+        </div>
 
-				<div className="shop__container">
-					<h2>{t("ourBest")}</h2>
-					<h3>{t("ourPremium")}</h3>
-					<div className="shop__premium-beers">
-						{BEER_PREMIUM.map((i, index) => (
-							<ShopList i={i} key={index} setCart={setCart} cart={cart} />
-						))}
-					</div>
-				</div>
-			</div>
-		</MainLayout>
-	)
+        <div className="shop__container">
+          <h2>{t("ourPremium")}</h2>
+          <div className="shop__premium-beers">
+            {BEER_PREMIUM.map((i, index) => (
+              <ShopList i={i} key={index} setCart={setCart} cart={cart} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </MainLayout>
+  )
 }
 
 export default Shop
 
 export async function getServerSideProps(context) {
-	const { req } = context
-	const { origin } = absoluteUrl(req)
+  const { req } = context
+  const { origin } = absoluteUrl(req)
 
-	const baseApiUrl = `${origin}/api/about`
+  const baseApiUrl = `${origin}/api/about`
 
-	const { token } = getAppCookies(req)
-	const profile = token ? verifyToken(token.split(" ")[1]) : ""
+  const { token } = getAppCookies(req)
+  const profile = token ? verifyToken(token.split(" ")[1]) : ""
 
-	return {
-		props: {
-			baseApiUrl,
-			profile,
-		},
-	}
+  return {
+    props: {
+      baseApiUrl,
+      profile,
+    },
+  }
 }
