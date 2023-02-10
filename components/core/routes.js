e2a200453715088fc9511fc50e72d161e8a50535
Carlos Luján <lujanbenita@gmail.com>
2023-02-10 20:39:31 +0100

    refactor nav menu

---
+++ components/core/routes.js
@@ -0,0 +1,52 @@
+export const ROUTER = [
+	{
+		id: 1,
+		href: "/",
+		text: "Home",
+	},
+	{
+		id: 2,
+		href: "/our-beers",
+		text: "ourBeers",
+	},
+	{
+		id: 3,
+		href: "/premium-beers",
+		text: "premiumBeers",
+	},
+	{
+		id: 4,
+		href: "/jobs",
+		text: "jobs",
+	},
+	{
+		id: 5,
+		href: "/history",
+		text: "history",
+	},
+	{
+		id: 6,
+		href: "/wt",
+		text: "wt",
+	},
+	{
+		id: 6,
+		href: "/wt",
+		text: "wt",
+	},
+	{
+		id: 7,
+		href: "/contact",
+		text: "contact",
+	},
+	{
+		id: 8,
+		href: "/shop",
+		text: "shop",
+	},
+	{
+		id: 9,
+		href: "/checkout",
+		text: "checkout",
+	},
+]
